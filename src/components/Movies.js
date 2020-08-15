497e3346019e3aef795f986036cbacd784885650
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-08-15 10:55:23 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>Name: {movie.title}</h3>
+          <h4>Name: {movie.time}</h4>
+          <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+          </ul>
+        </div>
+      ))
+      }
     </div>
   );
 };
