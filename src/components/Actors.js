1ba8653f5a60b11fa0abf12b38279cf5be0b02d3
osha7 <ogroetz@gmail.com>
2020-08-18 13:41:13 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -7,8 +7,8 @@ const Actors = () => {
       <h1>Actors Page</h1>
       {actors.map( actor => (
         <div>
-          <h3>Name: {actor.name}</h3>
-          <h2>Movies:</h2>
+          <h2>Name: {actor.name}</h2>
+          <h3>Movies:</h3>
           <ul>
             {actor.movies.map( movie => (
               <li>{movie}</li>
